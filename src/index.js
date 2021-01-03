75607289ed7d035f6dd63afbcd253dc21803b0c8
= <gustavotropper@gmail.com>
2021-01-03 09:57:24 -0300

    http server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require("express")
 const app = express()
+const http = require("http")
 const bodyParser = require("body-parser")
 
 const config = require("../resource/config.json")
 const fs = require("fs")
 const path = require("path")
-
-const https = require("https")
-const http = require("http")
 
 require("./database/mongodb")
 
@@ -23,19 +21,9 @@ app.use("/success", require("./routes/success"))
 app.use("/info", require("./routes/info"))
 app.get("*", (req, res) => res.redirect("/"))
 
-const httpServer = http.createServer(app)
-const httpsServer = https.createServer({
-    key: fs.readFileSync("/etc/ssl/key.pem", "utf8"),
-    cert: fs.readFileSync("/etc/ssl/cert.pem", "utf8")
-}, app)
-
-httpServer.listen(8080, () => {
-    console.log("HTTP server listening on port 8080")
+const server = http.createServer(app)
+server.listen(config.port, () => {
+    console.log(`Aplicação online na porta ${config.port}`)
 })
-
-httpsServer.listen(80, () => {
-    console.log("HTTPS server listening on port 80")
-})
-
 
 //app.listen(config.port, () => console.log("Aplicação online!"))
